1f29609f8fc5d2dfd65b5dbc611420717247d735
Sebastien Jodogne <s.jodogne@gmail.com>
2023-11-13 18:25:53 +0100

    run SyncOrthancFolder.py
    --HG--
    branch : 0.6.1

--- Resources/Orthanc/Plugins/OrthancPluginCppWrapper.h
+++ Resources/Orthanc/Plugins/OrthancPluginCppWrapper.h
@@ -2,8 +2,8 @@
  * Orthanc - A Lightweight, RESTful DICOM Store
  * Copyright (C) 2012-2016 Sebastien Jodogne, Medical Physics
  * Department, University Hospital of Liege, Belgium
- * Copyright (C) 2017-2022 Osimis S.A., Belgium
- * Copyright (C) 2021-2022 Sebastien Jodogne, ICTEAM UCLouvain, Belgium
+ * Copyright (C) 2017-2023 Osimis S.A., Belgium
+ * Copyright (C) 2021-2023 Sebastien Jodogne, ICTEAM UCLouvain, Belgium
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -136,6 +136,8 @@ namespace OrthancPlugins
                                 const OrthancPluginHttpRequest* request);
 
   void SetGlobalContext(OrthancPluginContext* context);
+
+  void ResetGlobalContext();
 
   bool HasGlobalContext();
 
@@ -533,6 +535,11 @@ namespace OrthancPlugins
 
   bool RestApiGet(Json::Value& result,
                   const std::string& uri,
+                  bool applyPlugins);
+
+  bool RestApiGet(Json::Value& result,
+                  const std::string& uri,
+                  const std::map<std::string, std::string>& httpHeaders,
                   bool applyPlugins);
 
   bool RestApiGetString(std::string& result,
@@ -1259,6 +1266,11 @@ namespace OrthancPlugins
 
     ~DicomInstance();
 
+    const OrthancPluginDicomInstance* GetObject() const
+    {
+      return instance_;
+    }
+
     std::string GetRemoteAet() const;
 
     const void* GetBuffer() const
@@ -1312,6 +1324,11 @@ namespace OrthancPlugins
     static DicomInstance* Transcode(const void* buffer,
                                     size_t size,
                                     const std::string& transferSyntax);
+#endif
+
+#if ORTHANC_PLUGINS_VERSION_IS_ABOVE(1, 12, 1)
+    static DicomInstance* Load(const std::string& instanceId,
+                               OrthancPluginLoadDicomInstanceMode mode);
 #endif
   };
 
